3dccb80a0bd603e327d6d1fc4dffc93d43c4bb08
Rostmoment <110627353+Rostmoment@users.noreply.github.com>
2023-04-14 18:37:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import json
+jsondata = {
+"data": [],
+"textures": []
+}
+filename = input("Enter file name: ")
+with open(filename, "rb") as fh:
+	data = fh.read().hex()
+	picCount = int(data[10:12], 16)
+	parse = 7
+	parseWidth = 8
+	parseWidth2 = 9
+	parseHeight = 10
+	parseHeight2 = 11
+	for i in range(picCount):
+		fh.seek(parse)
+		pixelType = fh.read(1)
+		fh.seek(parseWidth)
+		width1 =  fh.read(1)
+		width1 = int.from_bytes(width1, byteorder="little")
+		width1 *= 256
+		fh.seek(parseWidth2)
+		width2 =  fh.read(1)
+		width2 = int.from_bytes(width2, byteorder="little")
+		fh.seek(parseHeight)
+		height1 =  fh.read(1)
+		height1 = int.from_bytes(height1, byteorder="little")
+		height1 *= 256
+		fh.seek(parseHeight2)
+		height2 =  fh.read(1)
+		height2 = int.from_bytes(height2, byteorder="little")
+		height = height1 + height2
+		width = width1 + width2
+		pixelType = int.from_bytes(pixelType, byteorder="little")
+		texture = {
+		"pixelType": pixelType,
+		"width": width,
+		"height": height
+		}
+		jsondata["textures"].append(texture)
+		parse += 6
+		parseHeight += 6
+		parseHeight2 += 6
+		parseWidth += 6
+		parseWidth2 += 6
+picData = {
+"pictureCount": picCount
+}
+jsondata["data"].append(picData)
+jsonfile = json.dumps(jsondata, indent=4)
+filename = filename.replace(".xcod", ".json")
+with open(filename, "w") as fh:
+	fh.write(jsonfile)
